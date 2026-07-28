df5a69e594a5c2d8e386f6c18e7e886518fc92dd
CodeHumps001 <codehumps00233@gmail.com>
2026-07-28 09:46:39 +0000

    fix: link

--- src/app/(public)/contact/page.tsx
+++ src/app/(public)/contact/page.tsx
@@ -36,14 +36,11 @@ export default function ContactPage() {
   const onSubmit = async (data: any) => {
     setSubmitError(null);
     try {
-      const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/v1/contact`,
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(data),
-        },
-      );
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/contact`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
 
       const result = await res.json();
 
